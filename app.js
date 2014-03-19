4af5c904f93107d3b4d5b1ac81a9ed1f81f70afc
Randy L <randay@gmail.com>
2014-03-19 13:19:31 -0400

    put in own api key, added npm run bundle task

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ map.setView([47.63, -122.32], 11);
 var attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>';
  
 // set the tiles the map will use
-var tiles = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png';
+var tiles = 'http://{s}.tile.cloudmade.com/f27c8674933c4188a9b5d3222a002db8/997/256/{z}/{x}/{y}.png';
  
 // create a tileLayer with the tiles, attribution
 var layer = L.tileLayer(tiles, {
